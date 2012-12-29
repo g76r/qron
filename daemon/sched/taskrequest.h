d6f648e59e59ff486bb3c3eaabc3c44a56b6bdc1
Grégoire Barbier <devel@g76r.eu>
2012-12-29 16:14:52 +0100

    fixed copyright headers

--- daemon/sched/taskrequest.h
+++ daemon/sched/taskrequest.h
@@ -9,7 +9,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  * You should have received a copy of the GNU Affero General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with qron. If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef TASKREQUEST_H
 #define TASKREQUEST_H
