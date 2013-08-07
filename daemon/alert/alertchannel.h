g76r/qron

commit c8ae40916817184e757d3168a25359fe63dd9095

alerts: reminders are now managed by alert channels
Therefore less mails are now sent if several alerts are reminded
for a long time.
4 reminders will now need 1 mail every 2 hours instead of 4
mails every 2 hours if the 4 reminded alerts were raised
independently.
Moreover reminders are included in a new alert or alert
cancellation mail if any.

daemon/alert/alertchannel.h
@@ -31,7 +31,7 @@ protected:
   QWeakPointer<Alerter> _alerter;
 
 public:
-  enum MessageType { Emit, Cancel, Remind };
+  enum MessageType { Emit, Cancel };
   explicit AlertChannel(QObject *parent = 0, QWeakPointer<Alerter> alerter
                         = QWeakPointer<Alerter>());
   /** Asynchronously call implementation of doSendMessage() within dedicated

daemon/alert/alerter.cpp
@@ -29,7 +29,6 @@
 
 Alerter::Alerter() : QObject(0), _thread(new QThread),
   _cancelDelay(ALERTER_DEFAULT_CANCEL_DELAY),
-  _remindFrequency(ALERTER_DEFAULT_REMIND_FREQUENCY),
   _minDelayBetweenSend(ALERTER_DEFAULT_MIN_DELAY_BETWEEN_SEND){
   _thread->setObjectName("AlerterThread");
   connect(this, SIGNAL(destroyed(QObject*)), _thread, SLOT(quit()));
@@ -100,9 +99,6 @@ bool Alerter::loadConfiguration(PfNode root) {
       _params.valueAsInt("canceldelay", ALERTER_DEFAULT_CANCEL_DELAY/1000)*1000;
   if (_cancelDelay < 1000) // hard coded 1 second minmimum
     _cancelDelay = ALERTER_DEFAULT_CANCEL_DELAY;
-  _remindFrequency =
-      _params.valueAsInt("remindfrequency",
-                         ALERTER_DEFAULT_REMIND_FREQUENCY/1000)*1000;
   _minDelayBetweenSend =
       _params.valueAsInt("mindelaybetweensend",
                          ALERTER_DEFAULT_MIN_DELAY_BETWEEN_SEND/1000)*1000;
@@ -145,32 +141,14 @@ void Alerter::doEmitAlertCancellation(QString alert) {
   foreach (AlertRule rule, _rules) {
     if (rule.patternRegExp().exactMatch(alert)) {
       //Log::debug() << "alert matching rule #" << n;
-      if (rule.notifyCancel())
-        sendMessage(Alert(alert, rule), AlertChannel::Cancel);
+      sendMessage(Alert(alert, rule), AlertChannel::Cancel);
       if (rule.stop())
         break;
     }
     ++n;
   }
   _soonCanceledAlerts.remove(alert);
-  _remindedAlerts.remove(alert);
   emit alertCanceled(alert);
-}
-
-void Alerter::doRemindAlert(QString alert) {
-  Log::debug() << "reminding alert " << alert;
-  int n = 0;
-  foreach (AlertRule rule, _rules) {
-    if (rule.patternRegExp().exactMatch(alert)) {
-      //Log::debug() << "alert matching rule #" << n;
-      if (rule.notifyReminder())
-        sendMessage(Alert(alert, rule), AlertChannel::Remind);
-      if (rule.stop())
-        break;
-    }
-    ++n;
-  }
-  _remindedAlerts.insert(alert, QDateTime::currentDateTime());
 }
 
 void Alerter::sendMessage(Alert alert, AlertChannel::MessageType type) {
@@ -194,7 +172,6 @@ void Alerter::doRaiseAlert(QString alert) {
     } else {
       QDateTime now(QDateTime::currentDateTime());
       _raisedAlerts.insert(alert, now);
-      _remindedAlerts.insert(alert, now);
       Log::debug() << "raising alert " << alert;
       emit alertRaised(alert);
       doEmitAlert(alert);
@@ -240,9 +217,4 @@ void Alerter::asyncProcessing() {
   foreach (QString alert, _soonCanceledAlerts.keys())
     if (_soonCanceledAlerts.value(alert) <= now)
       doEmitAlertCancellation(alert);
-  // process alerts reminders
-  foreach (QString alert, _raisedAlerts.keys())
-    if (!_soonCanceledAlerts.contains(alert)
-        && _remindedAlerts.value(alert).msecsTo(now) > _remindFrequency)
-      doRemindAlert(alert);
 }

daemon/alert/alerter.h
@@ -46,9 +46,7 @@ class Alerter : public QObject {
   QList<AlertRule> _rules;
   QHash<QString,QDateTime> _raisedAlerts; // alert + raise time
   QHash<QString,QDateTime> _soonCanceledAlerts; // alert + scheduled cancel time
-  QHash<QString,QDateTime> _remindedAlerts; // alert + last reminder time
   int _cancelDelay;
-  int _remindFrequency;
   int _minDelayBetweenSend;
   int _gracePeriodBeforeFirstSend;
 
@@ -136,7 +134,6 @@ private slots:
 private:
   Q_INVOKABLE void doEmitAlert(QString alert);
   void doEmitAlertCancellation(QString alert);
-  void doRemindAlert(QString alert);
   Q_INVOKABLE void doRaiseAlert(QString alert);
   Q_INVOKABLE void doCancelAlert(QString alert, bool immediately = false);
   inline void sendMessage(Alert alert, AlertChannel::MessageType type);

daemon/alert/logalertchannel.cpp
@@ -29,9 +29,6 @@ void LogAlertChannel::doSendMessage(Alert alert, MessageType type) {
   case Cancel:
     Log::log(alert.rule().cancelMessage(alert),
              Log::severityFromString(alert.rule().address()));
-    break;
-  case Remind:
-    ; // ignore reminders
   }
 
 }

daemon/alert/mailalertchannel.cpp
@@ -19,10 +19,15 @@
 #include <QThread>
 #include "alerter.h"
 
+// LATER replace this 60" ugly batch with _remindFrequency and make reminding no longer drift
+#define ASYNC_PROCESSING_INTERVAL 60000
+
 class MailAlertQueue {
 public:
   QString _address;
-  QList<Alert> _alerts, _cancellations, _reminders;
+  QList<Alert> _alerts, _cancellations;
+  QHash<QString,QDateTime> _lastReminded;
+  QMap<QString,Alert> _reminders;
   QDateTime _lastMail;
   bool _processingScheduled;
   MailAlertQueue(const QString address = QString()) : _address(address),
@@ -31,9 +36,16 @@ public:
 
 MailAlertChannel::MailAlertChannel(QObject *parent,
                                    QWeakPointer<Alerter> alerter)
-  : AlertChannel(parent, alerter), _mailSender(0) {
+  : AlertChannel(parent, alerter), _mailSender(0),
+    _remindFrequency(ALERTER_DEFAULT_REMIND_FREQUENCY),
+    _asyncProcessingTimer(new QTimer) {
   _thread->setObjectName("MailAlertChannelThread");
   qRegisterMetaType<MailAlertQueue>("MailAlertQueue");
+  connect(_asyncProcessingTimer, SIGNAL(timeout()),
+          this, SLOT(asyncProcessing()));
+  connect(this, SIGNAL(destroyed()),
+          _asyncProcessingTimer, SLOT(deleteLater()));
+  _asyncProcessingTimer->start(ASYNC_PROCESSING_INTERVAL);
 }
 
 void MailAlertChannel::setParams(ParamSet params) {
@@ -54,6 +66,11 @@ void MailAlertChannel::setParams(ParamSet params) {
   _reminderStyle = params.value("mail.reminderstyle", "background:#ffff80");
   _cancelStyle = params.value("mail.cancelstyle", "background:#8080ff");
   _enableHtmlBody = params.value("mail.enablehtml", "true") == "true";
+  _remindFrequency =
+      params.valueAsInt("remindfrequency",
+                        ALERTER_DEFAULT_REMIND_FREQUENCY/1000)*1000;
+  //_asyncProcessingTimer->start(_remindFrequency);
+  asyncProcessing();
   Log::debug() << "MailAlertChannel configured " << relay << " "
                << _senderAddress << " " << params.toString();
 }
@@ -77,12 +94,17 @@ void MailAlertChannel::doSendMessage(Alert alert, MessageType type) {
     switch (type) {
     case Emit:
       queue->_alerts.append(alert);
+      if (alert.rule().notifyCancel()
+          && alert.rule().notifyReminder()) {
+        queue->_reminders.insert(alert.id(), alert);
+        queue->_lastReminded.insert(alert.id(), QDateTime());
+      }
       break;
     case Cancel:
-      queue->_cancellations.append(alert);
-      break;
-    case Remind:
-      queue->_reminders.append(alert);
+      if (alert.rule().notifyCancel())
+        queue->_cancellations.append(alert);
+      queue->_reminders.remove(alert.id());
+      queue->_lastReminded.remove(alert.id());
     };
     if (!queue->_processingScheduled) {
       // wait for a while before sending a mail with only 1 alert, in case some
@@ -103,9 +125,25 @@ void MailAlertChannel::processQueue(const QVariant address) {
                     "no queue: " << address;
     return;
   }
+  QDateTime now = QDateTime::currentDateTime();
   queue->_processingScheduled = false;
-  if (queue->_alerts.size() || queue->_cancellations.size()
-      || queue->_reminders.size()) {
+  bool haveJob = false;
+  if (queue->_alerts.size() || queue->_cancellations.size())
+    haveJob = true;
+  else {
+    foreach (const QDateTime &dt, queue->_lastReminded.values())
+      if (dt.isValid() && dt.msecsTo(now) > _remindFrequency) {
+        haveJob = true;
+        break;
+      }
+  }
+  if (haveJob) {
+    QList<Alert> reminders;
+    foreach (const QString &id, queue->_reminders.keys()) {
+      if (queue->_lastReminded.value(id).isValid()) // ignore new alerts
+        reminders.append(queue->_reminders.value(id));
+      queue->_lastReminded.insert(id, now); // update all timestamps
+    }
     QString errorString;
     int ms = queue->_lastMail.msecsTo(QDateTime::currentDateTime());
     int minDelayBetweenSend = _alerter
@@ -117,7 +155,7 @@ void MailAlertChannel::processQueue(const QVariant address) {
       Log::debug() << "MailAlertChannel::processQueue trying to send alerts "
                       "mail to " << addr << ": " << queue->_alerts.size()
                    << " new alerts + " << queue->_cancellations.size()
-                   << " cancellations + " << queue->_reminders.size()
+                   << " cancellations + " << reminders.size()
                    << " reminders";
       QStringList recipients(addr);
       QString text, subject, boundary("THISISTHEMIMEBOUNDARY"), s;
@@ -126,7 +164,7 @@ void MailAlertChannel::processQueue(const QVariant address) {
       // headers
       if (queue->_alerts.size())
         subject = _alertSubject;
-      else if (queue->_reminders.size())
+      else if (reminders.size())
         subject = _reminderSubject;
       else
         subject = _cancelSubject;
@@ -140,7 +178,7 @@ void MailAlertChannel::processQueue(const QVariant address) {
       headers.insert("X-qron-cancellations-count",
                      QString::number(queue->_cancellations.size()));
       headers.insert("X-qron-reminders-count",
-                     QString::number(queue->_reminders.size()));
+                     QString::number(reminders.size()));
       // body
       html = "<html><head><title>"+subject+"</title></head><body>";
       if (!_webConsoleUrl.isEmpty()) {
@@ -155,7 +193,7 @@ void MailAlertChannel::processQueue(const QVariant address) {
           .append(" new emited alerts, ")
           .append(QString::number(queue->_cancellations.size()))
           .append(" alert cancellations and ")
-          .append(QString::number(queue->_reminders.size()))
+          .append(QString::number(reminders.size()))
           .append(" reminders.");
       text.append(s).append("\r\n\r\n");
       html.append("<p>").append(s).append("\n");
@@ -176,11 +214,11 @@ void MailAlertChannel::processQueue(const QVariant address) {
       }
       text.append("\r\nAlerts reminders (alerts still raised):\r\n\r\n");
       html.append("</ul>\n<p>Alerts reminders (alerts still raised):\n<ul>\n");
-      if (queue->_reminders.isEmpty()) {
+      if (reminders.isEmpty()) {
         text.append("(none)\r\n");
         html.append("<li>(none)\n");
       } else {
-        foreach (Alert alert, queue->_reminders) {
+        foreach (Alert alert, reminders) {
           s = alert.datetime().toString("yyyy-MM-dd hh:mm:ss,zzz");
           s.append(" ").append(alert.rule().reminderMessage(alert))
               .append("\r\n");
@@ -239,7 +277,6 @@ void MailAlertChannel::processQueue(const QVariant address) {
         Log::info() << "successfuly sent an alert mail to " << addr;
         queue->_alerts.clear();
         queue->_cancellations.clear();
-        queue->_reminders.clear();
         queue->_lastMail = QDateTime::currentDateTime();
       } else {
         Log::warning() << "cannot send mail alert to " << addr
@@ -257,4 +294,11 @@ void MailAlertChannel::processQueue(const QVariant address) {
   } else {
     Log::debug() << "MailAlertChannel::processQueue called for nothing";
   }
+}
+
+void MailAlertChannel::asyncProcessing() {
+  // trigger queue processing for every queue with reminders, to check their age
+  foreach(const MailAlertQueue *queue, _queues.values())
+    if (!queue->_processingScheduled && !queue->_reminders.isEmpty())
+      processQueue(queue->_address);
 }

daemon/alert/mailalertchannel.h
@@ -21,6 +21,7 @@
 
 class MailAlertQueue;
 class MailSender;
+class QTimer;
 
 /** Log channel that send alerts as mails.
  * It performs alerts (including cancellations and reminders) aggregation within
@@ -34,6 +35,8 @@ class MailAlertChannel : public AlertChannel {
   QString _senderAddress, _webConsoleUrl, _alertSubject, _reminderSubject,
   _cancelSubject, _alertStyle, _reminderStyle, _cancelStyle;
   bool _enableHtmlBody;
+  int _remindFrequency;
+  QTimer *_asyncProcessingTimer;
 
 public:
   explicit MailAlertChannel(QObject *parent = 0, QWeakPointer<Alerter> alerter
@@ -43,6 +46,9 @@ public:
 
 public slots:
   void setParams(ParamSet params);
+
+private slots:
+  void asyncProcessing();
 
 private:
   Q_INVOKABLE void processQueue(const QVariant address);

daemon/alert/udpalertchannel.cpp
@@ -23,8 +23,6 @@ UdpAlertChannel::UdpAlertChannel(QObject *parent, QWeakPointer<Alerter> alerter)
 }
 
 void UdpAlertChannel::doSendMessage(Alert alert, MessageType type) {
-  if (type == Remind)
-    return; // ignore reminders
   if (!_socket)
     _socket = new QUdpSocket(this);
   // LATER support IPv6 numeric addresses (they contain colons)

commit a573c48f06746d7c4f06b73ca5a8ef88d62cbc08

alert: no longer reminding emited but not raised alerts
fixing a bug introduced by recent commit c8ae40916817

daemon/alert/alertchannel.h
@@ -31,7 +31,7 @@ protected:
   QWeakPointer<Alerter> _alerter;
 
 public:
-  enum MessageType { Emit, Cancel };
+  enum MessageType { Emit, Raise, Cancel };
   explicit AlertChannel(QObject *parent = 0, QWeakPointer<Alerter> alerter
                         = QWeakPointer<Alerter>());
   /** Asynchronously call implementation of doSendMessage() within dedicated

daemon/alert/alerter.cpp
@@ -50,6 +50,7 @@ Alerter::Alerter() : QObject(0), _thread(new QThread),
   qRegisterMetaType<ParamSet>("ParamSet");
   qRegisterMetaType<QDateTime>("QDateTime");
   qRegisterMetaType<QStringList>("QStringList");
+  qRegisterMetaType<AlertChannel::MessageType>("AlertChannel::MessageType");
 }
 
 Alerter::~Alerter() {
@@ -114,38 +115,29 @@ bool Alerter::loadConfiguration(PfNode root) {
 }
 
 void Alerter::emitAlert(QString alert) {
-  QMetaObject::invokeMethod(this, "doEmitAlert", Q_ARG(QString, alert));
+  QMetaObject::invokeMethod(this, "doEmitAlert", Q_ARG(QString, alert),
+                            Q_ARG(AlertChannel::MessageType,
+                                  AlertChannel::Emit));
 }
 
-void Alerter::doEmitAlert(QString alert) {
-  Log::debug() << "emiting alert " << alert;
+void Alerter::doEmitAlert(QString alert, AlertChannel::MessageType type) {
+  Log::debug() << "emiting alert " << alert << " " << type;
   int n = 0;
   foreach (AlertRule rule, _rules) {
     if (rule.patternRegExp().exactMatch(alert)) {
       //Log::debug() << "alert matching rule #" << n;
       if (rule.stop())
         break;
-      sendMessage(Alert(alert, rule), AlertChannel::Emit);
+      sendMessage(Alert(alert, rule), type);
     }
     ++n;
   }
-  emit alertEmited(alert);
-}
-
-void Alerter::doEmitAlertCancellation(QString alert) {
-  Log::debug() << "emiting alert cancellation " << alert;
-  int n = 0;
-  foreach (AlertRule rule, _rules) {
-    if (rule.patternRegExp().exactMatch(alert)) {
-      //Log::debug() << "alert matching rule #" << n;
-      if (rule.stop())
-        break;
-      sendMessage(Alert(alert, rule), AlertChannel::Cancel);
-    }
-    ++n;
+  if (type == AlertChannel::Cancel) {
+    _soonCanceledAlerts.remove(alert);
+    emit alertCanceled(alert);
+  } else {
+    emit alertEmited(alert);
   }
-  _soonCanceledAlerts.remove(alert);
-  emit alertCanceled(alert);
 }
 
 void Alerter::sendMessage(Alert alert, AlertChannel::MessageType type) {
@@ -171,7 +163,7 @@ void Alerter::doRaiseAlert(QString alert) {
       _raisedAlerts.insert(alert, now);
       Log::debug() << "raising alert " << alert;
       emit alertRaised(alert);
-      doEmitAlert(alert);
+      doEmitAlert(alert, AlertChannel::Raise);
     }
   }
 }
@@ -190,7 +182,7 @@ void Alerter::doCancelAlert(QString alert, bool immediately) {
   if (immediately) {
     if (_raisedAlerts.contains(alert) || _soonCanceledAlerts.contains(alert)) {
       Log::debug() << "do cancel alert immediately: " << alert;
-      doEmitAlertCancellation(alert);
+      doEmitAlert(alert, AlertChannel::Cancel);
       _raisedAlerts.remove(alert);
     }
   } else {
@@ -213,5 +205,5 @@ void Alerter::asyncProcessing() {
   // process alerts cancellations after cancel delay
   foreach (QString alert, _soonCanceledAlerts.keys())
     if (_soonCanceledAlerts.value(alert) <= now)
-      doEmitAlertCancellation(alert);
+      doEmitAlert(alert, AlertChannel::Cancel);
 }

daemon/alert/alerter.h
@@ -132,8 +132,7 @@ private slots:
   void asyncProcessing();
 
 private:
-  Q_INVOKABLE void doEmitAlert(QString alert);
-  void doEmitAlertCancellation(QString alert);
+  Q_INVOKABLE void doEmitAlert(QString alert, AlertChannel::MessageType type);
   Q_INVOKABLE void doRaiseAlert(QString alert);
   Q_INVOKABLE void doCancelAlert(QString alert, bool immediately = false);
   inline void sendMessage(Alert alert, AlertChannel::MessageType type);

daemon/alert/logalertchannel.cpp
@@ -23,6 +23,7 @@ LogAlertChannel::LogAlertChannel(QObject *parent, QWeakPointer<Alerter> alerter)
 void LogAlertChannel::doSendMessage(Alert alert, MessageType type) {
   switch(type) {
   case Emit:
+  case Raise:
     Log::log(alert.rule().emitMessage(alert),
              Log::severityFromString(alert.rule().address()));
     break;

daemon/alert/mailalertchannel.cpp
@@ -92,13 +92,14 @@ void MailAlertChannel::doSendMessage(Alert alert, MessageType type) {
       _queues.insert(address, queue);
     }
     switch (type) {
-    case Emit:
-      queue->_alerts.append(alert);
-      if (alert.rule().notifyCancel()
-          && alert.rule().notifyReminder()) {
+    case Raise:
+      if (alert.rule().notifyReminder()) {
         queue->_reminders.insert(alert.id(), alert);
         queue->_lastReminded.insert(alert.id(), QDateTime());
       }
+      // fall into next case
+    case Emit:
+      queue->_alerts.append(alert);
       break;
     case Cancel:
       if (alert.rule().notifyCancel())

daemon/examples/alerts1.conf
@@ -68,8 +68,10 @@
         (noremindernotify)
    )
   (rule(match config.**)
-        #(mail(address gb@localhost, root@localhost))
-        (stop)
+       (mail(address gb@localhost, root@localhost))
+  )
+  (rule(match config.**)
+       (stop)
   )
    (rule(match task.failure.app1.batch.*) # task.failure.%!fqtn
         (udp(address 127.0.0.1:4242)