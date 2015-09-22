9c2dc1aad5633f0c274603805dd4c29550ba581a
Grégoire Barbier <devel@g76r.eu>
2015-09-22 16:15:17 +0200

    copyright notices: s/qron.hallowyn.com/qron.eu/g

--- libqron/config/alertsubscription.h
+++ libqron/config/alertsubscription.h
@@ -1,5 +1,5 @@
 /* Copyright 2012-2015 Hallowyn and others.
- * This file is part of qron, see <http://qron.hallowyn.com/>.
+ * This file is part of qron, see <http://qron.eu/>.
  * Qron is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
